d08dda632ed06b884bd7e83a64b329e67a030717
王先生 <1324771795@qq.com>
2024-12-13 23:42:04 +0800

    添加项目文件。

---
+++ TestEditor/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace TestEditor;
+
+public partial class App : Application
+{
+}
---
+++ TestEditor/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace TestEditor;
+
+public partial class MainWindow : FluentWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        SystemThemeWatcher.Watch(this);
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Maximized;
+    }
+}
