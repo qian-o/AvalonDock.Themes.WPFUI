0c2de7ddbf74beb7338a6ce847552f7c3d2afb0f
王先生 <1324771795@qq.com>
2024-12-18 14:06:16 +0800

    更新 AvalonDock.Themes.WPFUI 项目资源和样式
    这些更改主要涉及到 AvalonDock.Themes.WPFUI 项目中的一些资源和样式的更新。
    在 `AvalonDock.Themes.WPFUI.csproj` 文件中，删除了对 PNG 资源的引用。
    删除了多个 PNG 文件，包括 `DockBottom.PNG`、`DockLeft.PNG`、`DockPane.PNG` 等。
    在 `Theme.xaml` 文件中，添加了对 `DockTargetButton.xaml` 资源字典的引用，并定义了两个新的 `PathGeometry` 资源。
    更新了 `OverlayWindow` 控件模板中的 `PART_PreviewBox` 的样式，改变了填充颜色、边框颜色和边框厚度。
    将 `PART_DockingManagerDropTargets`、`PART_AnchorablePaneDropTargets` 和 `PART_DocumentPaneDropTargets` 中的 `Image` 控件替换为 `DockTargetButton` 控件。
    添加了一个新的 `PART_DocumentPaneFullDropTargets` 网格，用于显示完整的文档面板拖放目标。
    新增了 `DockTargetButton.cs` 文件，定义了 `DockTargetButton` 控件及其相关的依赖属性和事件处理逻辑。
    `DockTargetButton` 控件支持不同的停靠目标（如 `DockLeft`、`DockTop`、`DockRight`、`DockBottom` 等），并在加载和卸载时管理这些控件的集合。
    `DockTargetButton` 控件还处理鼠标位置的变化，以确定当前的拖放目标，并更新预览框的可见性。
    
    该更改在 `DockTargetButton.xaml` 文件中添加了一个新的资源字典。
    资源字典定义了一个针对 `controls:DockTargetButton` 类型的样式。
    样式中包含了一些资源设置，如 `Canvas`、`Path` 和 `Line` 的样式设置。
    样式设置了 `DockTargetButton` 的一些属性，如 `Margin`、`Padding`、`Width`、`Height`、`BorderThickness`、`Opacity`、`CornerRadius`、`SnapsToDevicePixels` 和 `UseLayoutRounding`。
    样式还设置了 `Background`、`BorderBrush`、`GlyphBackground`、`GlyphBorderBrush`、`GlyphArrowBrush`、`OuterBackground` 和 `OuterBorderBrush` 的动态资源。
    样式包含多个触发器，根据 `IsTargeted` 和 `TargetDock` 属性的值来更改 `DockTargetButton` 的外观。
    触发器根据 `TargetDock` 的不同值（如 `Center`、`SplitLeft`、`SplitTop`、`SplitRight`、`SplitBottom`、`DockLeft`、`DockTop`、`DockRight` 和 `DockBottom`）设置不同的控件模板。
    多重触发器根据 `TargetDock` 和 `IsOuter` 的组合条件来设置 `DockTargetButton` 的 `Height`、`Width` 和控件模板。

---
+++ src/AvalonDock.Themes.WPFUI/Controls/DockTargetButton.cs
@@ -0,0 +1,230 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using AvalonDock.Controls;
+
+namespace AvalonDock.Themes.WPFUI.Controls;
+
+public enum DockTarget
+{
+    Center,
+    SplitLeft,
+    SplitTop,
+    SplitRight,
+    SplitBottom,
+    DockLeft,
+    DockTop,
+    DockRight,
+    DockBottom
+}
+
+public partial class DockTargetButton : Button
+{
+    private struct PointI
+    {
+        public int X;
+        public int Y;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern bool GetCursorPos(out PointI lpPoint);
+
+    public static readonly DependencyProperty TargetDockProperty;
+    public static readonly DependencyProperty CornerRadiusProperty;
+    public static readonly DependencyProperty GlyphBorderBrushProperty;
+    public static readonly DependencyProperty GlyphBackgroundProperty;
+    public static readonly DependencyProperty OuterBorderBrushProperty;
+    public static readonly DependencyProperty OuterBackgroundProperty;
+    public static readonly DependencyProperty GlyphArrowBrushProperty;
+    public static readonly DependencyProperty IsTargetedProperty;
+    public static readonly DependencyProperty IsOuterProperty;
+
+    private static readonly List<DockTargetButton> dockTargets = new();
+
+    private static Path? previewBox;
+    private static DockTargetButton? current;
+
+    static DockTargetButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(DockTargetButton), new FrameworkPropertyMetadata(typeof(DockTargetButton)));
+
+        TargetDockProperty = DependencyProperty.Register(nameof(TargetDock),
+                                                         typeof(DockTarget),
+                                                         typeof(DockTargetButton),
+                                                         new FrameworkPropertyMetadata(DockTarget.Center));
+
+        CornerRadiusProperty = DependencyProperty.Register(nameof(CornerRadius),
+                                                           typeof(CornerRadius),
+                                                           typeof(DockTargetButton),
+                                                           new FrameworkPropertyMetadata(new CornerRadius(0)));
+
+        GlyphBorderBrushProperty = DependencyProperty.Register(nameof(GlyphBorderBrush),
+                                                               typeof(Brush),
+                                                               typeof(DockTargetButton),
+                                                               new FrameworkPropertyMetadata(Brushes.Transparent));
+
+        GlyphBackgroundProperty = DependencyProperty.Register(nameof(GlyphBackground),
+                                                              typeof(Brush),
+                                                              typeof(DockTargetButton),
+                                                              new FrameworkPropertyMetadata(Brushes.Transparent));
+
+        OuterBorderBrushProperty = DependencyProperty.Register(nameof(OuterBorderBrush),
+                                                               typeof(Brush),
+                                                               typeof(DockTargetButton),
+                                                               new FrameworkPropertyMetadata(Brushes.Transparent));
+
+        OuterBackgroundProperty = DependencyProperty.Register(nameof(OuterBackground),
+                                                              typeof(Brush),
+                                                              typeof(DockTargetButton),
+                                                              new FrameworkPropertyMetadata(Brushes.Transparent));
+
+        GlyphArrowBrushProperty = DependencyProperty.Register(nameof(GlyphArrowBrush),
+                                                              typeof(Brush),
+                                                              typeof(DockTargetButton),
+                                                              new FrameworkPropertyMetadata(Brushes.Transparent));
+
+        IsTargetedProperty = DependencyProperty.Register(nameof(IsTargeted),
+                                                         typeof(bool),
+                                                         typeof(DockTargetButton),
+                                                         new FrameworkPropertyMetadata(false));
+
+        IsOuterProperty = DependencyProperty.Register(nameof(IsOuter),
+                                                      typeof(bool),
+                                                      typeof(DockTargetButton),
+                                                      new FrameworkPropertyMetadata(false));
+    }
+
+    public DockTargetButton()
+    {
+        Loaded += OnLoaded;
+        Unloaded += OnUnloaded;
+    }
+
+    public DockTarget TargetDock
+    {
+        get => (DockTarget)GetValue(TargetDockProperty);
+        set => SetValue(TargetDockProperty, value);
+    }
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    public Brush GlyphBorderBrush
+    {
+        get => (Brush)GetValue(GlyphBorderBrushProperty);
+        set => SetValue(GlyphBorderBrushProperty, value);
+    }
+
+    public Brush GlyphBackground
+    {
+        get => (Brush)GetValue(GlyphBackgroundProperty);
+        set => SetValue(GlyphBackgroundProperty, value);
+    }
+
+    public Brush GlyphArrowBrush
+    {
+        get => (Brush)GetValue(GlyphArrowBrushProperty);
+        set => SetValue(GlyphArrowBrushProperty, value);
+    }
+
+    public Brush OuterBorderBrush
+    {
+        get => (Brush)GetValue(OuterBorderBrushProperty);
+        set => SetValue(OuterBorderBrushProperty, value);
+    }
+
+    public Brush OuterBackground
+    {
+        get => (Brush)GetValue(OuterBackgroundProperty);
+        set => SetValue(OuterBackgroundProperty, value);
+    }
+
+    public bool IsTargeted
+    {
+        get => (bool)GetValue(IsTargetedProperty);
+        set => SetValue(IsTargetedProperty, value);
+    }
+
+    public bool IsOuter
+    {
+        get => (bool)GetValue(IsOuterProperty);
+        set => SetValue(IsOuterProperty, value);
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (previewBox is null)
+        {
+            if (sender is DockTargetButton dockTargetButton && dockTargetButton.TemplatedParent is OverlayWindow overlayWindow)
+            {
+                if (overlayWindow.Template.FindName("PART_PreviewBox", overlayWindow) is Path element)
+                {
+                    previewBox = element;
+                    previewBox.IsVisibleChanged += OnIsVisibleChanged;
+                }
+            }
+        }
+
+        dockTargets.Add(this);
+    }
+
+    private void OnUnloaded(object sender, RoutedEventArgs e)
+    {
+        if (previewBox is not null)
+        {
+            previewBox.IsVisibleChanged -= OnIsVisibleChanged;
+            previewBox = null;
+        }
+
+        dockTargets.Remove(this);
+    }
+
+    private static void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if ((bool)e.NewValue)
+        {
+            foreach (var item in dockTargets)
+            {
+                Rect rect = new(0,
+                                0,
+                                item.RenderSize.Width + 2,
+                                item.RenderSize.Height + 2);
+
+                Point point = item.PointFromScreen(GetMousePosition());
+
+                if (rect.Contains(point))
+                {
+                    current = item;
+
+                    current.IsTargeted = true;
+
+                    return;
+                }
+            }
+        }
+        else
+        {
+            if (current is not null)
+            {
+                current.IsTargeted = false;
+
+                current = null;
+            }
+        }
+    }
+
+    private static Point GetMousePosition()
+    {
+        if (GetCursorPos(out PointI point))
+        {
+            return new Point(point.X, point.Y);
+        }
+
+        return default;
+    }
+}
