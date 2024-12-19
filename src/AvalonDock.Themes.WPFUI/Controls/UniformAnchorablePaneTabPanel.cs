e2ceeca6f0c91a99695f19dfc6960a4c0b21abeb
王先生 <1324771795@qq.com>
2024-12-19 17:01:04 +0800

    更新 AvalonDock 布局控件和修复 XAML 标签
    在 `LayoutAnchorablePaneControl.xaml` 文件中，添加了 `xmlns:controls="clr-namespace:AvalonDock.Themes.WPFUI.Controls"` 命名空间。
    将 `AnchorablePaneTabPanel` 替换为 `UniformAnchorablePaneTabPanel`，并更新了相关的 XAML 标签。
    在 `LayoutDocumentPaneControl.xaml` 文件中，删除了一个未闭合的 `</Grid>` 标签。
    新增了 `UniformAnchorablePaneTabPanel.cs` 文件，定义了 `UniformAnchorablePaneTabPanel` 类，该类继承自 `AnchorablePaneTabPanel` 并重写了 `MeasureOverride` 和 `ArrangeOverride` 方法，以实现均匀分布子元素的功能。

---
+++ src/AvalonDock.Themes.WPFUI/Controls/UniformAnchorablePaneTabPanel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using AvalonDock.Controls;
+
+namespace AvalonDock.Themes.WPFUI.Controls
+{
+    public class UniformAnchorablePaneTabPanel : AnchorablePaneTabPanel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            double width = availableSize.Width;
+            double height = availableSize.Height;
+            UIElement[] enumerable = All().ToArray();
+
+            if (double.IsInfinity(width) || double.IsInfinity(height) || enumerable.Length is 0)
+            {
+                return base.MeasureOverride(availableSize);
+            }
+
+            double enumerableWidth = width / enumerable.Length;
+
+            foreach (var item in enumerable)
+            {
+                item.Measure(new Size(enumerableWidth, height));
+
+                height = Math.Max(height, item.DesiredSize.Height);
+            }
+
+            return new Size(width, height);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            double width = finalSize.Width;
+            UIElement[] enumerable = All().ToArray();
+
+            if (double.IsInfinity(width) || enumerable.Length is 0)
+            {
+                return base.MeasureOverride(finalSize);
+            }
+
+            double enumerableWidth = width / enumerable.Length;
+
+            double offset = 0.0;
+
+            foreach (var item in enumerable)
+            {
+                item.Arrange(new Rect(offset, 0.0, enumerableWidth, finalSize.Height));
+
+                offset += enumerableWidth;
+            }
+
+            return finalSize;
+        }
+
+        private IEnumerable<UIElement> All()
+        {
+            foreach (UIElement ch in Children)
+            {
+                if (ch.Visibility != Visibility.Collapsed)
+                {
+                    yield return ch;
+                }
+            }
+        }
+    }
+}
