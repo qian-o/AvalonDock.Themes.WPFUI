c9ae0e731533c070193bd621577759f302a4f597
王先生 <1324771795@qq.com>
2025-01-02 11:41:06 +0800

    重构主题管理逻辑
    将主题管理逻辑从 `App.xaml.cs` 移动到 `MainViewModel.cs` 中。
    删除了 `App.xaml.cs` 中的 `OnStartup` 方法及其内部的主题同步逻辑。
    在 `MainViewModel.cs` 中，添加了构造函数和析构函数，用于订阅和取消订阅 `ApplicationThemeManager.Changed` 事件。
    添加了 `ApplicationThemeManager_Changed` 方法，用于处理主题变化事件。
    更新了 `IsLightTheme` 属性的 `set` 方法，以应用新的主题并调用 `ThemeManager.Current.ChangeThemeBaseColor` 方法。

--- src/ExampleApp/App.xaml.cs
+++ src/ExampleApp/App.xaml.cs
@@ -1,24 +1,8 @@
 ﻿using System.Windows;
-using System.Windows.Media;
-using ControlzEx.Theming;
-using Wpf.Ui.Appearance;
 
 namespace ExampleApp
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
-
-            ApplicationThemeManager.Changed += ApplicationThemeManager_Changed;
-        }
-
-        private void ApplicationThemeManager_Changed(ApplicationTheme currentApplicationTheme, Color systemAccent)
-        {
-            ThemeManager.Current.SyncTheme();
-        }
     }
 }
--- src/ExampleApp/MainViewModel.cs
+++ src/ExampleApp/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using ControlzEx.Theming;
 using Wpf.Ui.Appearance;
@@ -8,6 +9,16 @@ namespace ExampleApp
     public class MainViewModel : ObservableRecipient
     {
         private bool isLightTheme = true;
+
+        public MainViewModel()
+        {
+            ApplicationThemeManager.Changed += ApplicationThemeManager_Changed;
+        }
+
+        ~MainViewModel()
+        {
+            ApplicationThemeManager.Changed -= ApplicationThemeManager_Changed;
+        }
 
         public bool IsLightTheme
         {
@@ -19,9 +30,17 @@ namespace ExampleApp
                     SetProperty(ref isLightTheme, value);
 
                     ApplicationThemeManager.Apply(value ? ApplicationTheme.Light : ApplicationTheme.Dark);
+
                     ThemeManager.Current.ChangeThemeBaseColor(Application.Current, value ? "Light" : "Dark");
                 }
             }
+        }
+
+        private void ApplicationThemeManager_Changed(ApplicationTheme currentApplicationTheme, Color systemAccent)
+        {
+            IsLightTheme = currentApplicationTheme == ApplicationTheme.Light;
+
+            ThemeManager.Current.SyncTheme(ThemeSyncMode.SyncAll);
         }
     }
 }
